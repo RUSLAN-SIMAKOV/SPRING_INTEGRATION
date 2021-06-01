00a20f56e81f111f54c3237536deb5c0595c8b0c
RUSLAN SIMAKOV <UA667766706@gmail.com>
2021-06-01 21:58:58 +0300

    Initial commit

---
+++ src/main/java/ruslan/simakov/springintegration/SpringintegrationApplication.java
@@ -0,0 +1,13 @@
+package ruslan.simakov.springintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringintegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringintegrationApplication.class, args);
+    }
+
+}
